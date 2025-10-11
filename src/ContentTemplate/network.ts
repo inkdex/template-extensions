a53fd0c63aec825612e29b0a307cd59a3592b389
Eduard Smet <contact@celarye.dev>
2025-10-11 12:01:14 +0200

    chore(ContentTemplate): Guidelines update

---
+++ src/ContentTemplate/network.ts
@@ -0,0 +1,19 @@
+import { PaperbackInterceptor, Request, Response } from "@paperback/types";
+
+// Intercepts all the requests and responses and allows you to make changes to them
+export class MainInterceptor extends PaperbackInterceptor {
+    override async interceptRequest(request: Request): Promise<Request> {
+        return request;
+    }
+
+    override async interceptResponse(
+        request: Request,
+        response: Response,
+        data: ArrayBuffer,
+    ): Promise<ArrayBuffer> {
+        void request;
+        void response;
+
+        return data;
+    }
+}
