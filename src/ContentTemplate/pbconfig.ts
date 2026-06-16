ef57d045d879ca477e830b406423dde879a2ffcd
Nicholas Lim <6513413+niclimcy@users.noreply.github.com>
2026-06-16 22:14:43 +0800

    feat: Replace deprecated code with new functions

--- src/ContentTemplate/pbconfig.ts
+++ src/ContentTemplate/pbconfig.ts
@@ -10,11 +10,12 @@ export default {
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
-  capabilities:
-    SourceIntents.SETTINGS_FORM_PROVIDING |
-    SourceIntents.DISCOVER_SECTION_PROVIDING |
-    SourceIntents.SEARCH_RESULT_PROVIDING |
+  capabilities: [
+    SourceIntents.SETTINGS_FORM_PROVIDING,
+    SourceIntents.DISCOVER_SECTION_PROVIDING,
+    SourceIntents.SEARCH_RESULT_PROVIDING,
     SourceIntents.CHAPTER_PROVIDING,
+  ],
   badges: [],
   developers: [
     {
